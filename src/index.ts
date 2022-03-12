5ce45b4e82175b4f1be572e30a85e6925482bcf0
sokintun7339656 <club_1303@mail.ru>
2022-03-12 19:05:55 +0500

    6-dars

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,96 @@
-import prompt_sync from "prompt-sync";
-const prompt = prompt_sync()
+// let a: number = 15
 
-const value = prompt('Enter value:')
 
-console.log("your value: " + value);
+
+// //functions
+// function suroj(a:number) {
+//   return a * a
+// }
+
+
+// let res = suroj(5)
+
+// console.log(res);
+
+
+
+// let d: number = (2)
+
+// function any(a:number, b:number, c:number) {
+  
+//  return (a + b) ** c
+// }
+
+// let ser = any(2,2,4)
+
+// console.log(ser);
+
+
+
+// let h: number = (3)
+// let j: number = (2)
+
+// function anagul(a:number, b:number) {
+//   return Math.max(a,b)
+// }
+
+// let gap = anagul(3,2)
+
+// console.log(gap);
+
+
+// let q: number = (5)
+
+
+// function opositi(a:number) {
+//   return a % 2 ==  0
+// }
+
+// let git = opositi(4)
+
+// console.log(git);
+
+
+// let e: string = ('Boltaboy')
+// let w: string = ('Muratov')
+
+// function nu(a:string, b:string) {
+//   return a + b
+// }
+
+// let kij = nu('Boltaboy ', ' Muratov')
+
+// console.log(kij);
+
+
+
+// let l: string = ('welcome')
+
+// function oloh(a:string) {
+//   return 'welcome ' + a
+// }
+
+// let b = oloh('Boltaboy')
+
+// console.log(b);
+
+
+
+
+
+// let p: string = ('olqosh')
+// console.log(p); 
+
+
+
+
+
+
+// function lk  (a:number) {
+//   for (let index = 0; index < a; index++) {
+//     console.log('salom');
+    
+    
+//   }
+// }
+// lk(10) 
